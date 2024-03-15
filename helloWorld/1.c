e42346710327d70ac72092bc93eee45f4bd22caa
SanjanaSK03 <01fe22bei004@kletech.ac.in>
2024-03-15 20:43:36 +0530

    File Handling

---
+++ helloWorld/1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define max 25
+int main()
+{
+    char a[max];
+    FILE *input,*output;
+    input=fopen("input.txt","r");
+    if(input==NULL)
+    {
+        perror("\nError in opening a file\n");
+        return 1;
+    }
+    int i=0;
+    int j=0;
+    while(!feof(input))
+    {
+        fscanf(input,"%c",&a[i]);
+        i++;
+    }
+    for(j=0;j<i-1;j++)
+    {
+        printf("%c\n",a[j]);
+    }
+
+    output=fopen("output.txt","w");
+    for(j=0;j<i;j++)
+    {
+        fprintf(output,"%c",a[j]);
+    }
+    fclose(output);
+    return 0;
+}
